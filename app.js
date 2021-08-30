c86237cec452cad364628e29bbc51dc6c021954f
Jacob Deal <jadeal1515@gmail.com>
2021-08-30 14:22:23 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(numbers) {
+  let sum = 0;
+  numbers.forEach((num) => {
+    sum += num;
+  });
+  return sum;
+}
+console.log(arraySum(numbers))
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
-
+const book = {};
+book.title = "Enders Game"
+book.pages = 324;
+book.readCount = 2;
+book.info = () => {
+    return `${book.title}, ${book.pages} pages, Read ${book.readCount} times.`;
+};
+console.log(book.info());
